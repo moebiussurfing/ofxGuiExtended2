1c04e3028621bd3af25ae81c414e3a8fc9082ca7
moebiussurfing <moebiussurfing@users.noreply.github.com>
2020-05-06 21:30:31 -0300

    started adding addGroup for adding nested ofParameterGroup

--- exampleParameterGroup/src/ofApp.cpp
+++ exampleParameterGroup/src/ofApp.cpp
@@ -3,52 +3,68 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofSetFrameRate(25);
-	renderer1.setup("r1");
-	renderer2.setup("r2");
+
+	//renderer1.setup("r1");
+	//renderer2.setup("r2");
 
 	parameters.setName("g1");
 	parameters.add(vSync.set("b",true));
+	   
+    //parameters2.setName("g2");
+    //parameters2.add(vSync.set("b",true));
 
-
-
-    parameters2.setName("g2");
-    parameters2.add(vSync.set("b",true));
-//    parameters2.add(renderer1.parameters);
-    parameters2.add(renderer2.parameters);
-//    parameters.add(parameters2);
+////    parameters2.add(renderer1.parameters);
+//    parameters2.add(renderer2.parameters);
+////    parameters.add(parameters2);
 
     panel = gui.addPanel(parameters);
-    panel->getVisible().set(false);
+//    panel->getVisible().set(false);
+//	
+//    group = gui.addGroup(parameters);
+//    //group1 = group->addGroup(renderer1.parameters);
+//    group2 = group->addGroup(parameters2);
+////    auto group3 = group2->addGroup(parameters);
+	   
+
+	//----
+//
+//
+//
+//	//TODO:
+//    parameters3.setName("g3");
+//    parameters3.add(parameters);
+//    //parameters3.add(parameters2);
+//    //parameters3.add(renderer1.parameters);
+//    //parameters3.add(renderer2.parameters);
+//
+////    auto gnames = parameters3.getGroupHierarchyNames();
+//    auto gnames = parameters3.getGroup("g3").getGroupHierarchyNames();
+//    cout <<"gnames:"<< ofToString(gnames) << endl;
 
 
-    group = gui.addGroup(parameters);
-    group1 = group->addGroup(renderer1.parameters);
-    group2 = group->addGroup(parameters2);
-//    auto group3 = group2->addGroup(parameters);
+
+	//myGroup = gui.addGroup("myGroup");
 
 
 
-    parameters3.setName("g3");
-    parameters3.add(parameters);
-    parameters3.add(parameters2);
-    parameters3.add(renderer1.parameters);
-    parameters3.add(renderer2.parameters);
 
 
-//    auto gnames = parameters3.getGroupHierarchyNames();
-    auto gnames = parameters3.getGroup("g3").getGroupHierarchyNames();
-    cout <<"gnames:"<< ofToString(gnames) << endl;
 
 
-    group->setPosition(0, 0);
-	panel->loadFromFile("settings.xml");
-	font.load( OF_TTF_SANS,9,true,true);
+	//----
+
+
+    //group->setPosition(0, 0);
+	//panel->loadFromFile("settings.xml");
+	//font.load(OF_TTF_SANS, 9, true, true);
+	//font.load("assets/fonts/DINPro-Bold.otf", 9, true, true);
+
 	ofEnableAlphaBlending();
 
 
 //    group = gui.addPanel("AUDIO FACTORY");
-    group->loadTheme("assets/themes/theme_bleurgh.json");
-    group->setPosition(0,0);
+    //group->loadTheme("assets/themes/theme_bleurgh.json");
+    //group->setPosition(0,0);
 }
 
 void ofApp::vSyncChanged(bool & vSync){
@@ -66,51 +82,52 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
 	ofBackgroundGradient(ofColor::white, ofColor::gray);
-	renderer1.draw();
-	renderer2.draw();
-	ofSetColor(255);
-	font.drawString("frame: " + ofToString(renderer1.frameNum),ofGetWidth()-150,20);
-	font.drawString("fps: " + ofToString((int)ofGetFrameRate()),ofGetWidth()-150,40);
+
+	//renderer1.draw();
+	//renderer2.draw();
+	//ofSetColor(255);
+	//font.drawString("frame: " + ofToString(renderer1.frameNum),ofGetWidth()-150,20);
+	//font.drawString("fps: " + ofToString((int)ofGetFrameRate()),ofGetWidth()-150,40);
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-	if(key=='s'){
-		ofSerialize(settings, parameters);
-		settings.save("settings.xml");
-	}
-	if(key=='l'){
-		settings.load("settings.xml");
-		ofDeserialize(settings, parameters);
-	}
-	if(key=='o'){
-		cout << renderer1.parameters;
-		cout << renderer2.parameters;
-	}
-	if(key=='r'){
-		renderer1.color = ofColor(127);
-		renderer2.color = ofColor(127);
-	}
-    if(key=='1'){
-
-        group2->minimize();
-    }
-    if(key=='2'){
-
-        group2->maximize();
-    }
-    if(key=='3'){
-         auto r1 = group->getGroup("r1");
-         r1->getGroup("position")->setEnabled(false);
-    }
-    if(key=='4'){
-        auto r1 = group->getGroup("r1");
-        r1->getGroup("position")->setEnabled(true);
-    }
-    if(key=='5'){
+//	if(key=='s'){
+//		ofSerialize(settings, parameters);
+//		settings.save("settings.xml");
+//	}
+//	if(key=='l'){
+//		settings.load("settings.xml");
+//		ofDeserialize(settings, parameters);
+//	}
+//	if(key=='o'){
+//		cout << renderer1.parameters;
+//		cout << renderer2.parameters;
+//	}
+//	if(key=='r'){
+//		renderer1.color = ofColor(127);
+//		renderer2.color = ofColor(127);
+//	}
+//    if(key=='1'){
+//
+//        group2->minimize();
+//    }
+//    if(key=='2'){
+//
+//        group2->maximize();
+//    }
+//    if(key=='3'){
+//         auto r1 = group->getGroup("r1");
+//         r1->getGroup("position")->setEnabled(false);
+//    }
+//    if(key=='4'){
 //        auto r1 = group->getGroup("r1");
-//        r1->
-    }
+//        r1->getGroup("position")->setEnabled(true);
+//    }
+//    if(key=='5'){
+////        auto r1 = group->getGroup("r1");
+////        r1->
+//    }
 
 }
 
@@ -163,3 +180,132 @@ void ofApp::gotMessage(ofMessage msg){
 void ofApp::dragEvent(ofDragInfo dragInfo){
 
 }
+
+
+//
+////--------------------------------------------------------------
+//template<typename ParameterType>
+//bool ofApp::AddParameter(ofParameter<ParameterType>& parameter)
+//{
+//	//auto tmpRef = parameter.get();
+//	//const auto& info = typeid(ParameterType);
+//
+//	////convert string to char pointer
+//	//string _str = parameter.getName();
+//	//const char * _name(_str.c_str());
+//
+//
+//	////_name -> GetUniqueName(parameter)
+//	//if (info == typeid(float))
+//	//{
+//	//	if (ImGui::SliderFloat(GetUniqueName(parameter), (float *)&tmpRef, parameter.getMin(), parameter.getMax()))
+//	//	{
+//	//		parameter.set(tmpRef);
+//	//		return true;
+//	//	}
+//	//	return false;
+//	//}
+//	//if (info == typeid(int))
+//	//{
+//	//	if (ImGui::SliderInt(GetUniqueName(parameter), (int *)&tmpRef, parameter.getMin(), parameter.getMax()))
+//	//	{
+//	//		parameter.set(tmpRef);
+//	//		return true;
+//	//	}
+//	//	return false;
+//	//}
+//	//if (info == typeid(bool))
+//	//{
+//	//	if (ImGui::Checkbox(GetUniqueName(parameter), (bool *)&tmpRef))
+//	//	{
+//	//		parameter.set(tmpRef);
+//	//		return true;
+//	//	}
+//	//	return false;
+//	//}
+//
+//	ofLogWarning(__FUNCTION__) << "Could not create GUI element for type " << info.name();
+//	return false;
+//}
+//
+////--------------------------------------------------------------
+//void ofApp::AddGroup(ofParameterGroup& group)
+//{
+//	for (auto parameter : group)
+//	{
+//		// Group.
+//		auto parameterGroup = std::dynamic_pointer_cast<ofParameterGroup>(parameter);
+//		if (parameterGroup)
+//		{
+//			// Recurse through contents.
+//			AddGroup(*parameterGroup);
+//			continue;
+//		}
+//
+////		// Parameter, try everything we know how to handle.
+////#if OF_VERSION_MINOR >= 10
+////		auto parameterVec2f = std::dynamic_pointer_cast<ofParameter<glm::vec2>>(parameter);
+////		if (parameterVec2f)
+////		{
+////			ofxImGui::AddParameter(*parameterVec2f);
+////			continue;
+////		}
+////		auto parameterVec3f = std::dynamic_pointer_cast<ofParameter<glm::vec3>>(parameter);
+////		if (parameterVec3f)
+////		{
+////			ofxImGui::AddParameter(*parameterVec3f);
+////			continue;
+////		}
+////		auto parameterVec4f = std::dynamic_pointer_cast<ofParameter<glm::vec4>>(parameter);
+////		if (parameterVec4f)
+////		{
+////			ofxImGui::AddParameter(*parameterVec4f);
+////			continue;
+////		}
+////#endif
+////		auto parameterOfVec2f = std::dynamic_pointer_cast<ofParameter<ofVec2f>>(parameter);
+////		if (parameterOfVec2f)
+////		{
+////			ofxImGui::AddParameter(*parameterOfVec2f);
+////			continue;
+////		}
+////		auto parameterOfVec3f = std::dynamic_pointer_cast<ofParameter<ofVec3f>>(parameter);
+////		if (parameterOfVec3f)
+////		{
+////			ofxImGui::AddParameter(*parameterOfVec3f);
+////			continue;
+////		}
+////		auto parameterOfVec4f = std::dynamic_pointer_cast<ofParameter<ofVec4f>>(parameter);
+////		if (parameterOfVec4f)
+////		{
+////			ofxImGui::AddParameter(*parameterOfVec4f);
+////			continue;
+////		}
+////		auto parameterFloatColor = std::dynamic_pointer_cast<ofParameter<ofFloatColor>>(parameter);
+////		if (parameterFloatColor)
+////		{
+////			ofxImGui::AddParameter(*parameterFloatColor);
+////			continue;
+////		}
+////		auto parameterFloat = std::dynamic_pointer_cast<ofParameter<float>>(parameter);
+////		if (parameterFloat)
+////		{
+////			ofxImGui::AddParameter(*parameterFloat);
+////			continue;
+////		}
+////		auto parameterInt = std::dynamic_pointer_cast<ofParameter<int>>(parameter);
+////		if (parameterInt)
+////		{
+////			ofxImGui::AddParameter(*parameterInt);
+////			continue;
+////		}
+////		auto parameterBool = std::dynamic_pointer_cast<ofParameter<bool>>(parameter);
+////		if (parameterBool)
+////		{
+////			ofxImGui::AddParameter(*parameterBool);
+////			continue;
+////		}
+//
+//		ofLogWarning(__FUNCTION__) << "Could not create GUI element for parameter " << parameter->getName();
+//	}
+//}
--- exampleParameterGroup/src/ofApp.h
+++ exampleParameterGroup/src/ofApp.h
@@ -1,46 +1,53 @@
 #pragma once
 
 #include "ofMain.h"
-#include "ofxGuiExtended.h"
-#include "CirclesRenderer.h"
+#include "ofxGuiExtended2.h"
+//#include "CirclesRenderer.h"
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp {
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+	void setup();
+	void update();
+	void draw();
 
-		void keyPressed  (int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
 
-		void vSyncChanged(bool & vSync);
+	void vSyncChanged(bool & vSync);
 
-		ofxGui gui;
-		ofxGuiPanel* panel;
-    ofxGuiGroup* group;
-    ofxGuiGroup* group1;
-    ofxGuiGroup* group2;
-    ofxGuiGroup* group3;
+	ofxGui gui;
+	ofxGuiPanel* panel;
+	ofxGuiGroup2* group;
+	ofxGuiGroup2* group1;
+	ofxGuiGroup2* group2;
+	ofxGuiGroup2* group3;
 
 
-    ofParameter<bool> vSync;
+	ofParameter<bool> vSync;
 
-    ofParameterGroup parameters;
-    ofParameterGroup parameters2;
-ofParameterGroup parameters3;
+	ofParameterGroup parameters;
+	//ofParameterGroup parameters2;
+	//ofParameterGroup parameters3;
 
-    ofXml settings;
+	ofXml settings;
 
-    CirclesRenderer renderer1,renderer2;
-		ofTrueTypeFont font;
+	//CirclesRenderer renderer1, renderer2;
+	//ofTrueTypeFont font;
+
+
+	////TODO:
+	//ofxGuiGroup2* myGroup;
+	//template<typename ParameterType>
+	//bool AddParameter(ofParameter<ParameterType>& parameter);
+	//void AddGroup(ofParameterGroup& group);
 };
